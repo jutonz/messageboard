b1df40941debcc1a6c10d1e09804f378b963ae1b
Jeremy W. Rowe <jeremy.w.rowe@gmail.com>
2014-07-28 21:44:24 -0400

    test posts, a little

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,7 +1,25 @@
 require 'test_helper'
 
 class PostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def post
+    @post ||= posts(:justins)
+  end
+
+  test "the post belongs to the correct user" do
+    assert post.user == users(:justin)
+  end
+
+  test "#created_by returns the formatted user name" do
+    assert_equal "Justin Toniazzo", post.created_by
+  end
+
+  test "is not valid when the title is longer than 100 characters" do
+    post.title = "*" * 101
+    refute post.valid?
+  end
+
+  test "is valid when the title is 100 or less long" do
+    post.title = "*" * 100
+    assert post.valid?
+  end
 end
